2fbeb7ca2e2112415c93e346041fa3a9fb29b2b7
Nikhil Sunil Waghmode <nick13waghmode@gmail.com>
2023-06-08 00:34:05 +0530

    Update Heap_Sort.java

--- Heap_Sort.java
+++ Heap_Sort.java
@@ -67,3 +67,12 @@ class dsa_9_heapsort {
         System.out.println();
     }
 }
+
+/*First convert the array into heap data structure using heapify, then one by one delete the root node of the Max-heap and replace it with the last node in the heap and then heapify the root of the heap. Repeat this process until size of heap is greater than 1.
+
+Build a heap from the given input array.
+Repeat the following steps until the heap contains only one element:
+Swap the root element of the heap (which is the largest element) with the last element of the heap.
+Remove the last element of the heap (which is now in the correct position).
+Heapify the remaining elements of the heap.
+The sorted array is obtained by reversing the order of the elements in the input array.*/
